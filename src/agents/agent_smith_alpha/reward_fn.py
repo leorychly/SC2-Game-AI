28d508197b5e4b638dc22d471114f1c2cde3794a
Leonard <leonard.rychly@gmx.net>
2019-12-02 22:24:32 +0100

    added some reward functions

--- src/agents/agent_smith_alpha/reward_fn.py
+++ src/agents/agent_smith_alpha/reward_fn.py
@@ -7,28 +7,44 @@ https://github.com/deepmind/pysc2/blob/master/pysc2/lib/features.py
 
 
 class BaseRewardFn(ABC):
-
   def __call__(self, obs):
     return self._compute_reward(obs)
-
   @abstractmethod
   def _compute_reward(self, obs):
     raise NotImplementedError
 
 
 class SparseRewardFn(BaseRewardFn):
-
   def _compute_reward(self, obs):
     reward = obs.reward
     return reward
 
 
-class KillScoreRewardFn(BaseRewardFn):
+class ScoreRewardFn(BaseRewardFn):
+  def _compute_reward(self, obs):
+    reward = obs.observation["score_cumulative"][0]  # total score
+    return reward / 10000
 
+
+class KillScoreRewardFn(BaseRewardFn):
   def _compute_reward(self, obs):
     reward = 0
     reward += obs.observation["score_cumulative"][5]  # killed_value_units
     reward += obs.observation["score_cumulative"][6]  # killed_value_structures
     reward += obs.observation["score_by_category"][1].army  # killed_minerals
     reward += obs.observation["score_by_category"][1].economy  # killed_minerals
-    return reward
+    return reward / 10000
+
+
+class ComprehensiveScoreRewardFn(BaseRewardFn):
+  def _compute_reward(self, obs):
+    reward = 0
+    reward += obs.observation["score_cumulative"][3]  # total_value_units
+    reward += obs.observation["score_cumulative"][5]  # killed_value_units
+    reward += obs.observation["score_cumulative"][6]  # killed_value_structures
+    reward += obs.observation["score_cumulative"][7]  # collected_minerals
+    reward += obs.observation["score_by_category"][1]  # killed_minerals
+    reward -= obs.observation["score_by_category"][3]  # lost_minerals
+    reward += obs.observation["score_categories"][1]  # army
+    reward += obs.observation["score_categories"][2]  # economy
+    return reward / 10000
